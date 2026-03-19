58c3e82f634f8a065e612b555937f4ab9771ed17
Sam <you@example.comsam@ballyhoo.co.uk>
2026-03-19 17:12:55 +0000

    Refine custom footer layout and cycling

--- 2-sam-custom-footer.lua
+++ 2-sam-custom-footer.lua
@@ -1,10 +1,4 @@
-local Device = require("device")
 local ReaderFooter = require("apps/reader/modules/readerfooter")
-local TextWidget = require("ui/widget/textwidget")
-local userpatch = require("userpatch")
-
-local Screen = Device.screen
-local footerTextGeneratorMap = userpatch.getUpValue(ReaderFooter.applyFooterMode, "footerTextGeneratorMap")
 
 local cycle_setting_key = "sam_custom_footer_cycle_mode"
 local cycle_modes = {
@@ -44,11 +38,17 @@ local function get_right_text(footer)
         if footer.settings.pages_left_includes_current_page then
             left = left + 1
         end
-        return ("%d left ch"):format(left)
+        return ("%d left in ch"):format(left)
     end
 
     if mode == "pages_left_book" then
-        local left = footer.pages - footer.pageno
+        local left
+        if footer.ui.pagemap and footer.ui.pagemap:wantsPageLabels() then
+            local _, idx, count = footer.ui.pagemap:getCurrentPageLabel(false)
+            left = count - idx
+        else
+            left = footer.pages - footer.pageno
+        end
         if footer.settings.pages_left_includes_current_page then
             left = left + 1
         end
@@ -58,66 +58,71 @@ local function get_right_text(footer)
     return ("%." .. footer.settings.progress_pct_format .. "f%%"):format(footer.percent_finished * 100)
 end
 
-local function build_custom_footer_text(footer)
-    local max_width = math.floor(footer._saved_screen_width - 2 * footer.horizontal_margin)
-    local right_text = get_right_text(footer)
-    local title = footer.ui.doc_props.display_title or footer.ui.doc_props.title or ""
+local function ensure_custom_footer_layout(footer)
+    footer.settings.disable_progress_bar = false
+    footer.settings.progress_bar_position = "below"
+    footer.settings.progress_style_thin = true
+    footer.settings.items_separator = "bar"
+    footer.settings.align = "center"
+    footer.settings.all_at_once = true
 
-    local right_widget = TextWidget:new{
-        text = right_text,
-        face = footer.footer_text_face,
-        bold = footer.settings.text_font_bold,
+    footer.settings.page_progress = false
+    footer.settings.pages_left_book = false
+    footer.settings.pages_left = false
+    footer.settings.percentage = false
+    footer.settings.book_title = false
+    footer.settings.book_author = false
+    footer.settings.bookmark_count = false
+    footer.settings.time = false
+    footer.settings.battery = false
+    footer.settings.book_time_to_read = false
+    footer.settings.chapter_time_to_read = false
+    footer.settings.frontlight = false
+    footer.settings.mem_usage = false
+    footer.settings.wifi_status = false
+    footer.settings.page_turning_inverted = false
+    footer.settings.chapter_progress = false
+    footer.settings.custom_text = false
+
+    footer.settings.book_chapter = true
+    footer.settings.dynamic_filler = true
+    footer.settings.additional_content = true
+    footer.settings.book_chapter_max_width_pct = 72
+    footer.settings.progress_margin_width = 10
+    footer.settings.container_bottom_padding = 1
+    footer.settings.bottom_horizontal_separator = false
+    footer.settings.order = {
+        [0] = "book_chapter",
+        [1] = "dynamic_filler",
+        [2] = "additional_content",
     }
-    local right_width = right_widget:getSize().w
-    right_widget:free()
 
-    local space_widget = TextWidget:new{
-        text = " ",
-        face = footer.footer_text_face,
-        bold = footer.settings.text_font_bold,
-    }
-    local space_width = space_widget:getSize().w
-    space_widget:free()
+    if not footer._sam_custom_footer_content then
+        footer._sam_custom_footer_content = function()
+            return get_right_text(footer)
+        end
+        footer:addAdditionalFooterContent(footer._sam_custom_footer_content)
+    end
 
-    local title_limit = math.max(0, max_width - right_width - (space_width * 2))
-    local title_limit_pct = math.max(10, math.floor((title_limit / max_width) * 100))
-    local fitted_title = footer:getFittedText(title, title_limit_pct)
-
-    local title_widget = TextWidget:new{
-        text = fitted_title,
-        face = footer.footer_text_face,
-        bold = footer.settings.text_font_bold,
-    }
-    local title_width = title_widget:getSize().w
-    title_widget:free()
-
-    local filler_spaces = math.max(2, math.floor((max_width - title_width - right_width) / space_width))
-
-    return fitted_title .. (" "):rep(filler_spaces) .. right_text
+    footer:set_mode_index()
+    footer:set_has_no_mode()
+    footer:updateFooterTextGenerator()
+    footer:applyFooterMode(footer.mode_list.book_chapter)
 end
 
 local orig_init = ReaderFooter.init
 function ReaderFooter:init(...)
     orig_init(self, ...)
-
-    self.settings.disable_progress_bar = false
-    self.settings.progress_bar_position = "above"
-    self.settings.progress_style_thin = true
-    self.settings.items_separator = "none"
-    self.settings.align = "left"
-    self.settings.all_at_once = false
-    self.settings.book_title = false
-    self.settings.page_progress = true
-
-    self:applyFooterMode(self.mode_list.page_progress)
+    ensure_custom_footer_layout(self)
 end
 
-local orig_genAllFooterText = ReaderFooter.genAllFooterText
-function ReaderFooter:genAllFooterText(generator, ...)
-    if generator == nil or generator == footerTextGeneratorMap.page_progress then
-        return build_custom_footer_text(self), false
-    end
-    return orig_genAllFooterText(self, generator, ...)
+local orig_onReaderReady = ReaderFooter.onReaderReady
+function ReaderFooter:onReaderReady(...)
+    orig_onReaderReady(self, ...)
+    ensure_custom_footer_layout(self)
+    self:updateFooterContainer()
+    self:resetLayout(true)
+    self:onUpdateFooter(true)
 end
 
 local orig_TapFooter = ReaderFooter.TapFooter
@@ -130,7 +135,7 @@ function ReaderFooter:TapFooter(ges)
         return true
     end
 
-    if ges.pos.x >= Screen:getWidth() * 0.65 then
+    if ges.pos.x >= (self._saved_screen_width or 0) * 0.65 then
         local next_index = (get_cycle_index() % #cycle_modes) + 1
         save_cycle_index(next_index)
         self:onUpdateFooter(true)
